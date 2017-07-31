d6ff815555ce8ac420c0e9dee09b02d821438a2b
Rui Fang <ruifang0126@gmail.com>
2017-07-31 00:19:09 -0700

    add riot.txt

--- routers/router.js
+++ routers/router.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 
+router.get('/riot.txt', function(request, response) {
+  response.sendFile('riot.txt', { root: path.join(__dirname, "../") });
+});
+
 router.get('/', function (request, response) {
 	response.sendFile('index.html', { root: path.join(__dirname, "../public/views/") });
 });
